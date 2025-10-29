9a4c28c92f01d28ac6bb62cac0c52a9b0dcaa675
memegodmidaswastaken-lang <memegodmidaswastaken@gmail.com>
2025-10-29 18:53:25 -0500

    Update addReview.js

--- netlify/functions/addReview.js
+++ netlify/functions/addReview.js
@@ -1,18 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
-const supabase = createClient(
-  process.env.https://rcfftyvlmqxjdpxxiapl.supabase.co,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZmZ0eXZsbXF4amRweHhpYXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3Njc2MzEsImV4cCI6MjA3NzM0MzYzMX0.zyXjgwYdyYYMy94EChbWioasrUWRsx8CPuShkRa-3ms
-);
-
-export async function handler(event) {
+exports.handler = async (event) => {
   const { house_id, user_id, review } = JSON.parse(event.body);
 
-  const { data, error } = await supabase
-    .from('reviews')
-    .insert([{ house_id, user_id, review }]);
-
+  const { data, error } = await supabase.from('reviews').insert([{ house_id, user_id, review }]);
   if (error) return { statusCode: 400, body: JSON.stringify({ error: error.message }) };
 
   return { statusCode: 200, body: JSON.stringify(data) };
-}
+};
